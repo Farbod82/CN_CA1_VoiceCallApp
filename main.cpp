6efd201ffbdae46966899e4212be1258c9237b3f
Farbod82 <farbodazimmohseni@gmail.com>
2024-03-22 10:53:53 +0330

    implemented a simple tcp client server for test as a base for signaling server

--- main.cpp
+++ main.cpp
@@ -5,13 +5,52 @@
 #include <QAudioFormat>
 #include <QAudioInput>
 #include <QThread>
+#include <rtc/rtc.h>
+#include <boost/asio.hpp>
+
+// #include "stunserver.cpp"
+// #include "stun_client.cpp"
+#include "listner.h"
+#include "signaling_server.cpp"
+#include "test_client.cpp"
 
 
+void runServer2(){
+    boost::asio::io_context io_context;
+    // we'll just use an arbitrary port here
+    Listner s(io_context, 8080);
+    // and we run until our server is alive
+    io_context.run();
+}
 int main(int argc, char *argv[])
 {
+
     QApplication a(argc, argv);
-    AudioRecorder ar;
-    ar.record();
+    QThread serverThread;
+    QThread clientThread;
+    QThread clientThread2;
+
+    QObject::connect(&serverThread, &QThread::started, [](){
+        runServer2();
+    });
+
+    QObject::connect(&clientThread, &QThread::started, [](){
+        runClient2();
+    });
+    QObject::connect(&clientThread2, &QThread::started, [](){
+        runClient2();
+    });
+
+    serverThread.start();
+    clientThread.start();
+
+    // QThread::msleep(10000);
+    clientThread2.start();
+
+    // AudioRecorder ar;
+    // ar.record();
+
+
     MainWindow w;
     w.show();
     return a.exec();
