6f204714b5e4dda13818893a47597f92ba883868
SeyyedAhmadRokni <seyyedahmadrokni@ut.ac.ir>
2024-04-02 17:49:13 +0330

    some codes for making datachannel, oppening connection and first socket for communication added. must debug

--- connection.h
+++ connection.h
@@ -3,12 +3,16 @@
 
 #include <QObject>
 #include "audiorecorder.h"
+#include "rtc/rtc.hpp"
 
 class connection : public QObject
 {
+    Q_OBJECT
 public:
     explicit connection(QObject* parent = nullptr);
+    ~connection();
     void init();
+    void make_data_channel();
 signals:
     void audioReceived(const QByteArray& audioData);
 private slots:
@@ -16,6 +20,7 @@ private slots:
 private:
     rtc::Configuration config;
     rtc::PeerConnection pc;
+    rtc::DataChannel dc;
     AudioRecorder audio_recorder;
 };
 
